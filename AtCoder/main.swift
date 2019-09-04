c5fc0d7a33e2bfb3dc092a2c13cc38477b5a3b05
asashin227 <asa.shin.asa@gmail.com>
2019-09-04 19:35:33 +0900

    Initial Commit

---
+++ AtCoder/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AtCoder
+//
+//  Created by Asakura Shinsuke on 2019/09/04.
+//  Copyright © 2019 asashin227. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
